d105e22d87e0ee3527b84e7b58298a9b7668ece7
Markus Zehnder <markus.z@unfoldedcircle.com>
2022-12-24 15:33:48 +0100

    fixup! docs: initial Dock-API release

--- dock-api/create-html-docker.sh
+++ dock-api/create-html-docker.sh
@@ -6,4 +6,4 @@ mkdir -p "$OUT_DIR"
 docker run --rm -it \
   -v "${OUT_DIR}":/app/example \
   -v "${PWD}/UCD2-asyncapi.yaml":/app/asyncapi.yaml \
-  asyncapi/generator:1.9.14 asyncapi.yaml @asyncapi/html-template@0.28 --force-write -o example
+  asyncapi/generator:1.9.2 asyncapi.yaml @asyncapi/html-template@0.25 --force-write -o example
--- integration-api/create-html-docker.sh
+++ integration-api/create-html-docker.sh
@@ -6,4 +6,4 @@ mkdir -p "$OUT_DIR"
 docker run --rm -it \
   -v "${OUT_DIR}":/app/example \
   -v "${PWD}/asyncapi.yaml":/app/asyncapi.yaml \
-  asyncapi/generator:1.9.2 asyncapi.yaml @asyncapi/html-template --force-write -o example
+  asyncapi/generator:1.9.2 asyncapi.yaml @asyncapi/html-template@0.25 --force-write -o example
