1ed9d594e09da5c5e18688ac2c3e6dc3132412f8
zehnm <business@markuszehnder.ch>
2026-05-13 20:50:41 +0000

    Deploying to gh-pages from @ unfoldedcircle/core-api@c02f34203cd7cf80cc3392285632e0e63536cab0 🚀

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-2f057a50.js',
+                path_to_root + 'searchindex-c81ebc12.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
